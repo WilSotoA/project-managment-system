ba8393e63e823bec2ee3a284f46ce40eaffb952e
WilSotoA <113364120+WilSotoA@users.noreply.github.com>
2023-10-02 16:26:22 -0500

    add-task-and-project-tables

--- database/migrations/2_10_02_210951_create_projects_table.php
+++ database/migrations/2_10_02_210951_create_projects_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,13 +12,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->timestamps();
-            $table->foreignId('role_id')->nullable()->constrained($table = 'roles');
+            $table->string('title');
+            $table->text('description');
+            $table->date('start_date')->default(Carbon::now());
+            $table->date('end_date');
         });
     }
 
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('projects');
     }
 };
